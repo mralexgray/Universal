cdab3e4532c8e22fe28980a680572dea46f14e73
Alex Gray <alex@mrgray.com>
2015-04-24 12:13:39 -0400

    framework sub-variation, rest is working pretty good.

--- XC_Install_Helper.zsh
+++ XC_Install_Helper.zsh
@@ -1,23 +1,26 @@
+#!/bin/sh
 
-NOTIFY() {
+NOTIFY() { # say "notify with $# args, exit status will be $2"
     
-    [[ $(type -f terminal-notifier) ]] && { terminal-notifier -title "$TARGET_NAME" -message "$1"; } || say "$1"
-    [[ $# > 2 ]] && exit $2
+    [[ $(type -f terminal-notifier) ]]  &&  terminal-notifier -title "$TARGET_NAME" -message "$1" || say "$1"
+
+    [[ "$#" == "2" ]] && exit $2
 }
 
 MAC_FW () {
-    
-  WRAPPED_NAME="${PROJECT_NAME}.framework"
-  FW_DIR="${USER_LIBRARY_DIR}/Frameworks"
-  [[ -z "$FW_DIR/$WRAPPED_NAME" ]] && NOTIFY "product isnt there"
 
-  if ! /usr/bin/diff -x 'Modules' -rq "${BUILT_PRODUCTS_DIR}/$WRAPPED_NAME" "$FW_DIR/$WRAPPER_NAME"
-  then NOTIFY "skipping install" 0
-  fi
+  [[ -d "${FW_DIR=${USER_LIBRARY_DIR}/Frameworks}/${WRAPPED_NAME=${PROJECT_NAME}.framework}"  && \
+     ! $(/usr/bin/diff -x 'Modules' -x ".DS_Store" -rq "${BUILT_PRODUCTS_DIR}/$WRAPPED_NAME" "$FW_DIR/$WRAPPER_NAME")  ]] && {
+			 
+	NOTIFY "skipping install" 0 
+	
+	} || {
+	
+		/usr/bin/rsync --delete --recursive --times -v --progress  --links "${BUILT_PRODUCTS_DIR}/$WRAPPED_NAME" "$FW_DIR"
   
-  /usr/bin/rsync --delete --recursive --times -v --progress  --links "${BUILT_PRODUCTS_DIR}/$WRAPPED_NAME" "$FW_DIR"
-  
-  NOTIFY "RSYNC'd ${WRAPPED_NAME}" 0
+      NOTIFY "RSYNC'd ${WRAPPED_NAME}" 0
+		}
+#  say "sybned framework" && exit 0
   #  /usr/bin/rsync --recursive --times -v --progress --links --stats "$PROD" "$USER_FWS"
 }
 
@@ -35,17 +38,19 @@ GO_SIM_FWK() {
 }
 
 [[ ! -x "${EXE=${CODESIGNING_FOLDER_PATH}/${TARGET_NAME}}" ]] && NOTIFY "Hmm, missing executable:$EXE"  36
-[[ ! $(otool -L "${EXE:-/dev/null}") ]]                        && NOTIFY "otool verify failed!"          37
+otool -L "$EXE"  || NOTIFY "otool verify failed!" 37
 
-[[ "${EFFECTIVE_PLATFORM_NAME}" =~ "mac"       ]]   && MAC_FW        || \
-[[          $WRAPPER_EXTENSION  != "framework" ]]   && GO_DEVICE_APP || \
-[[    $EFFECTIVE_PLATFORM_NAME  =~ "simulator"      &&   \
-           "$WRAPPER_EXTENSION" == "framework" ]]   && GO_SIM_FWK    || \
-[[    $EFFECTIVE_PLATFORM_NAME  != "-iphoneos" ]]   && NOTIFY "dunno what to do" 0
+
+if 	 [[ ! "$EFFECTIVE_PLATFORM_NAME" ]]; then MAC_FW
+elif [[      $WRAPPER_EXTENSION  != "framework" ]]; then GO_DEVICE_APP
+elif [[ $EFFECTIVE_PLATFORM_NAME  =~ "simulator"  &&  "$WRAPPER_EXTENSION" == "framework" ]]; then GO_SIM_FWK
+elif [[ $EFFECTIVE_PLATFORM_NAME  != "-iphoneos" ]]; then NOTIFY "dunno what to do" 0
+else
 
      HASH=$(md5 -q "$EXE")
   HASHKEY="$TARGET_NAME${EFFECTIVE_PLATFORM_NAME:--$PLATFORM_NAME}"
-SAVEDHASH="$(defaults read com.mrgray.AtoZ $HASHKEY)" 2> /dev/null; # check saved hash
+SAVEDHASH="$(defaults read com.mrgray.AtoZ $HASHKEY)" 2> /dev/null
+# check saved hash
 
 [[ "$HASH" == "$SAVEDHASH" ]] && NOTIFY "$TARGET_NAME hashes match!" 0
 
@@ -55,30 +60,9 @@ NOTIFY "Installing TARGET_NAME Framework onto device."
 
 scp -r  "$CODESIGNING_FOLDER_PATH" 6:/Library/Frameworks 2>&1 | head -n1 | sed 's/ssh://;s/6\.local.*//g'
 
-[[ $? == 0 ]] && NOTIFY "installed ${TARGET_NAME/#AtoZ/} framework on device" 
-              || NOTIFY "copy to device failed for ${TARGET_NAME/#AtoZ/}." 58
-}
+[[ $? != 0 ]] && NOTIFY "copy to device failed for ${TARGET_NAME/#AtoZ/}." 58
 
-#buildPlist="${PRODUCT_NAME}-Info.plist"
-## Get the existing buildVersion and buildNumber values from the buildPlist
-#buildVersion=$(/usr/libexec/PlistBuddy -c "Print CFBuildVersion" $buildPlist)
-#buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBuildNumber" $buildPlist)
-#buildDate=$(date "+%Y%m%d.%H%M%S")
+NOTIFY "installed ${TARGET_NAME/#AtoZ/} framework on device" 
 
-## Increment the buildNumber
-#buildNumber=$(($buildNumber + 1))
+fi
 
-## Set the version numbers in the buildPlist
-#/usr/libexec/PlistBuddy -c "Set :CFBuildNumber $buildNumber" $buildPlist
-#/usr/libexec/PlistBuddy -c "Set :CFBuildDate $buildDate" $buildPlist
-#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildVersion.$buildNumber" $buildPlist
-#/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $buildVersion.$buildNumber" $buildPlist
-#/usr/libexec/PlistBuddy -c "Set :CFBundleLongVersionString $buildVersion.$buildNumber.$buildDate" $buildPlist
-#+%s)
-#/usr/libexec/PlistBuddy -c "Set :__TIMESTAMP__ $(date)" "$INFOPLIST_FILE"
-# set -e
-# set -o pipefail
-#terminal-notifier -title "$TARGET_NAME" -message "no diff"
-#  --checksum  --sparse
-#  logger "rsync cmd is \"$RCMD\"" eval "$RCMD"
-#  defaults write "com.mrgray.AtoZ" "$HASHKEY" $HASH
