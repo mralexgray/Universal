861a0422b482951c4f2a88be761f0eb91af5e583
Alex Gray <alex@mrgray.com>
2015-06-02 16:39:56 -0400

    made commit script commit main repo too

--- recursive_commit.sh
+++ recursive_commit.sh
@@ -8,15 +8,19 @@
 
 GITROOT="$(git root)"
 
+doACP() { read acp; [[ -n "$acp" ]] && git acp "$acp"; }
+
 for x in $(git modules); do
 
   cd "$GITROOT/$x"
   if git dirty; then
     git status
     echo "ACP message for $(basename $GITROOT) submodule $x?"
-    read acp
-    [[ -n "$acp" ]] && git acp "$acp"
+    doACP
   else
     echo "submodule $x IS NOT dirty!"
   fi
 done
+
+echo "\n\n\n ACP for main Repo, at $GITROOT"
+doACP
